bd0e8915f07a9c3b474f8af05a7d248074b1f963
Oleg Golikov <oleg.golikov.06@mail.ru>
2026-03-30 21:11:25 +0300

    added .gitignore

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,8 @@
 def get_mask_card_number(card_num: str) -> str:
+    """Маскирует номер карты"""
     return f"{card_num[:4]} {card_num[4:6]}** **** {card_num[-4:]}"
 
 
 def get_mask_account(acc_num: str) -> str:
+    """Маскирует номер аккаунта"""
     return f"**{acc_num[-4:]}"
