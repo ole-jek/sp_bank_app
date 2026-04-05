11871216510957f80801bcfab3651eef6eaf49cf
Oleg Golikov <oleg.golikov.06@mail.ru>
2026-04-05 20:28:46 +0300

    added sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,17 @@
-def filter_by_state(id_list: list, state: str = 'EXECUTED') -> list:
+def filter_by_state(id_list: list, state: str = "EXECUTED") -> list:
+    """Возвращает новый лист со словарями соответствующими ключу"""
     new_list = []
     for item in id_list:
-        if item['state'] == state:
+        if item["state"] == state:
             new_list.append(item)
     return new_list
 
 
+def get_iem_date(item: dict) -> str:
+    """Вспомогательная функция для нахождения даты из словаря и передачи в функцию sorted"""
+    return item["date"]
+
+
+def sort_by_date(id_list: list, decreasing: bool = True) -> list:
+    """Сортировка списка по датам"""
+    return sorted(id_list, key=get_iem_date, reverse=decreasing)
