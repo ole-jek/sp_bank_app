2c747b0c62094a0dce9cb75ba03daf7f7aeacdd6
Oleg Golikov <oleg.golikov.06@mail.ru>
2026-04-14 01:21:10 +0300

    masks tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,7 +3,12 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def test_get_mask_account():
     assert get_mask_account("73654108430135874305") == "**4305"
+    assert get_mask_account("736541084305874305") == "**4305"
+    assert get_mask_account("") == ""
 
 
 def test_mask_card_number():
     assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
+    assert get_mask_card_number("4222222222222") == "4222 22** *222 2"
+    assert get_mask_card_number("6762123456789012345") == "6762 12** **** ***2 345"
+    assert get_mask_card_number("") == ""
